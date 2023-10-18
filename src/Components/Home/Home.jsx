38319cac7ff4968171e5fb274b8572a4bf0b4396
shihab <mdshihabali8888@gmail.com>
2023-10-18 19:20:17 +0600

    add to use loader data create my project

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { useLoaderData } from "react-router-dom";
+
+const Home = () => {
+    const tshirts =useLoaderData();
+    return (
+        <div>
+            <h1>This is Home:{tshirts.length}</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h1>This Is Order Review Page</h1>
+        </div>
+    );
+};
+
+export default OrderReview;
